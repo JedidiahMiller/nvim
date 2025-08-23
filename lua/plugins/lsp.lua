1ce62c22032648234ddc0c22281f8f9446800b52
JedidiahMiller <jedidiah@jwmiller.org>
2025-08-23 16:26:52 -0400

    lsp improvements

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,57 +1,99 @@
 return {
-    -- Generic highlighting support
+    -- Treesitter for syntax highlighting and code parsing
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        ensure_installed = "all",
-        highlight = { enable = true },
-        indent = { enable = true },
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
     },
 
-    -- Handles standalone stuff
+    -- Mason: Package manager for LSP servers, linters, and formatters
     {
         "mason-org/mason.nvim",
         opts = {
-        ensure_installed = {
-            "rust_analyzer",
-            "tsserver",
-            "html",
-            "cssls",
-            "tailwindcss",
-            "clangd",
-            "phpactor",
-            "pyright",
-        },
+            ensure_installed = {
+                -- LSP Servers
+                "rust_analyzer",              -- Rust
+                "vue-language-server",        -- Vue
+                "typescript-language-server", -- TypeScript/JavaScript
+                "html",                       -- HTML
+                "cssls",                      -- CSS
+                "tailwindcss",                -- Tailwind CSS
+                "clangd",                     -- C/C++
+                "phpactor",                   -- PHP
+                "pyright",                    -- Python
+                
+                -- Linters
+                "eslint",                     -- JavaScript/TypeScript
+                "phpcs",                      -- PHP
+                
+                -- Formatters
+                "prettier",                   -- JavaScript/TypeScript/CSS/HTML
+                "rustfmt",                    -- Rust
+                "black",                      -- Python
+            },
         }
     },
+
+    -- LSP Configuration (sets up the language servers)
     {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {"prettier", "eslint", "phpcs"},
+        "neovim/nvim-lspconfig",
         dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
         },
+        config = function()
+            require("mason").setup()
+            
+            -- Only install LSP servers here, not linters/formatters
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "rust_analyzer",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "clangd",
+                    "phpactor",
+                    "pyright",
+                },
+                automatic_installation = true,
+            })
+            
+            -- Basic LSP keymaps
+            local lsp = vim.lsp
+            local opts = { buffer = 0, silent = true }
+            
+            vim.keymap.set('n', 'gd', lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gr', lsp.buf.references, opts)
+            vim.keymap.set('n', 'K', lsp.buf.hover, opts)
+            vim.keymap.set('n', '<leader>rn', lsp.buf.rename, opts)
+            vim.keymap.set('n', '<leader>ca', lsp.buf.code_action, opts)
+        end,
     },
+
+    -- Completion engine
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
         },
         config = function()
-        local cmp = require("cmp")
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = {
-            { name = "nvim_lsp" },
-            { name = "buffer" },
-            { name = "path" },
-            },
-        })
+            local cmp = require("cmp")
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            })
         end,
     },
 }
